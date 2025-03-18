bfbbf29cfaf2a003ed69cb3e624d12554422f48c
pranav3850 <pranavgoswami38@gmail.com>
2025-03-17 23:47:23 -0400

    Initial commit

---
+++ src/main/java/org/example/c0928316_assignment_4/StudentManagementApplication.java
@@ -0,0 +1,11 @@
+package org.example.c0928316_assignment_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StudentManagementApplication.class, args);
+    }
+}
---
+++ src/test/java/org/example/c0928316_assignment_4/C0928316Assignment4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.c0928316_assignment_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C0928316Assignment4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
